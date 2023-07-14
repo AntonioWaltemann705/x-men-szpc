b7b07dac538f675a1b61f07e5973fbcc06992bed
Luis Antônio Gonçalves Waltemann <139577086+AntonioWaltemann705@users.noreply.github.com>
2023-07-14 14:45:57 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,43 @@
+const personagens = document.querySelectorAll('.personagem')
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+        
+        removerSelecaoDoPersonagem();
+        
+        personagem.classList.add('selecionado')
+
+        alterarImagemPersonagemSelecionado(personagem);
+
+        alterarNomePersonagemSelecionado(personagem);
+
+        alterarDescricaoPersonagemSelecionado(personagem);
+
+    })
+
+})
+
+function alterarDescricaoPersonagemSelecionado(personagem) {
+    const descricaoPersonagem = document.getElementById('descricao-personagem');
+    descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+}
+
+function alterarNomePersonagemSelecionado(personagem) {
+    const nomePersonagem = document.getElementById('nome-personagem');
+
+    nomePersonagem.innerText = personagem.getAttribute('data-name');
+}
+
+function alterarImagemPersonagemSelecionado(personagem) {
+    const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+
+    const idPersonagem = personagem.attributes.id.value;
+
+    imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+}
+
+function removerSelecaoDoPersonagem() {
+    const personagemSelecionado = document.querySelector('.selecionado');
+    personagemSelecionado.classList.remove('selecionado');
+}
+
